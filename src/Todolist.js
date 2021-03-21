7ac7761410a6164174e48b09be576e5fc093891c
yuxuchen <chenyuxuan0930@gmail.com>
2021-03-22 03:32:03 +1100

    first commit

---
+++ src/Todolist.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//create component
+class Todolist extends Component{
+    render(){
+        //write bracket to return multi-line commond statement
+        return(
+            <div><input/><button>提交</button></div>
+        )
+    }
+}
+
+//component can be referenced only if they are exported
+
+export default Todolist;
