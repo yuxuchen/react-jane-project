5f54730ba8530e0a5d110e4e622efe6c78d11579
yuxuchen <48660428+yuxuchen@users.noreply.github.com>
2021-03-23 02:02:36 +1100

    Add files via upload

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,11 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
+import './style.css'
+
 //create component
 class Todolist extends Component{
+    // class must have one constractor which to be excuted first
+    constructor(props){
+        super(props);
+        this.state={
+            inputValue:'',
+            list:[]
+        }
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemDelete=this.handleItemDelete.bind(this);
+    }
     render(){
         //write bracket to return multi-line commond statement
         return(
-            <div><input/><button>提交</button></div>
+            <Fragment>
+                <div>
+                    <label htmlFor="insertArea">输入内容</label>
+                    {/*下面是一个input框*/}
+                    <input 
+                    id="insertArea"
+                    className='input'
+                    value={this.state.inputValue}
+                    onChange={this.handleInputChange}
+                    />
+                    <button onClick={this.handleBtnClick}>提交</button>
+                </div>
+                <ul>
+                {this.getTodoItem()}
+            </ul>
+            </Fragment>
         )
+    }
+    getTodoItem(){
+        return this.state.list.map((item, index)=>{
+            return(
+                <TodoItem 
+                key={index}
+                content={item} 
+                index={index}
+                deleteItem={this.handleItemDelete}
+                />
+        )
+        })
+
+    }
+    handleInputChange(e){
+        const value=e.target.value;
+        this.setState(()=>({
+            inputValue:value
+        }))
+    }
+    handleBtnClick(){
+        this.setState((prevState)=>({
+            //...this.state.list 把以前的运算符展开再形成一个新的数组
+            list:[...prevState.list,prevState.inputValue],inputValue:''
+        }))
+    }
+    handleItemDelete(index){
+        //react是immutable，state不允许我们做任何改变，只能复制之后改变副本
+         //删除一个位于index的数据
+        this.setState((prevState)=>{
+            const list=[...prevState.list];
+            list.splice(index,1);
+            return {list}
+        });
     }
 }
 
