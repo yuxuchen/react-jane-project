2ae308c2bf9da2f6f018a793df2df384fef0f78a
yuxuchen <48660428+yuxuchen@users.noreply.github.com>
2021-03-26 02:42:00 +1100

    Add files via upload

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -9,6 +9,7 @@ class TodoItem extends Component{
         this.handleClick=this.handleClick.bind(this);
     }
     render(){
+        console.log('child render')
         const {content,test }=this.props;
         return (
         <div onClick={this.handleClick}>
@@ -21,13 +22,25 @@ class TodoItem extends Component{
         const {deleteItem, index} = this.props;
         deleteItem(index);
     }
+    //一个组件要从父组件接受参数
+    //只要父组件的render函数被重新执行了，子组件的生命周期函数就会被执行
+    //如果这个组件第一次存在于父组件中，不会执行
+    //如果这个组件之前已经存在于父组件中，才会执行
+    componentWillReceiveProps() {
+        console.log('child componentWillReciveProps');
+    }
+    //当这个组件即将被从页面移除的时候会执行
+    componentWillUnmount(){
+        console.log('child componentWillUnmount')
+    }
 }
+
 
 //propTypes在传值不对的时候，会给提示
 TodoItem.propTypes={
     test:PropTypes.string.isRequired,
     //必须传
-    content:PropTypes.string,
+    content:PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
     deleteItem:PropTypes.func,
     index:PropTypes.number
 }
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
-import Test from './Test';
 import './style.css'
 
 //create component
@@ -8,7 +7,7 @@ class Todolist extends Component{
     // class must have one constractor which to be excuted first
     constructor(props){
         super(props);
-        //当组件的state活着props发生改变的时候，render函数就会重新执行
+        //当组件的state或者props发生改变的时候，render函数就会重新执行
         this.state={
             inputValue:'',
             list:[]
@@ -17,6 +16,13 @@ class Todolist extends Component{
         this.handleBtnClick=this.handleBtnClick.bind(this);
         this.handleItemDelete=this.handleItemDelete.bind(this);
     }
+
+    //当组件即将被挂载到页面的时候会被执行
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+    
+    
     render(){
         console.log('render');
         //write bracket to return multi-line commond statement
@@ -30,16 +36,37 @@ class Todolist extends Component{
                     className='input'
                     value={this.state.inputValue}
                     onChange={this.handleInputChange}
+                    ref={(input) =>{this.input=input}}
                     />
                     <button onClick={this.handleBtnClick}>提交</button>
                 </div>
-                <ul>
+                <ul ref={(ul)=>this.ul=ul}>
                 {this.getTodoItem()}
             </ul>
-            <Test content={this.state.inputValue}/>
             </Fragment>
         )
     }
+
+    //组件被挂载在页面之后执行,只有在组件第一次被挂载的时候执行一次，之后并不会被执行
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    //组件被更新之前，他会自动被执行
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate');
+        return true;
+    }
+    //组件被更新之前，他会自动被执行,但是他在shouldComponentUpdate之后执行
+    //如果shouldComponentUpdate返回true它才执行
+    //如果返回false，这个函数就不会被执行了
+     componentWillUpdate(){
+        console.log('componentWillUpdate');
+    }
+    //组件更新完成之后会被执行
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+    
     getTodoItem(){
         return this.state.list.map((item, index)=>{
             return(
@@ -53,8 +80,8 @@ class Todolist extends Component{
         })
 
     }
-    handleInputChange(e){
-        const value=e.target.value;
+    handleInputChange(){
+        const value=this.input.value;
         this.setState(()=>({
             inputValue:value
         }))
@@ -62,8 +89,12 @@ class Todolist extends Component{
     handleBtnClick(){
         this.setState((prevState)=>({
             //...this.state.list 把以前的运算符展开再形成一个新的数组
-            list:[...prevState.list,prevState.inputValue],inputValue:''
-        }))
+            list:[...prevState.list,prevState.inputValue],
+            inputValue:''
+        }),()=>{
+            console.log(this.ul.querySelectorAll('div').length);
+        });
+        //setSate第二参数是一个回调函数
     }
     handleItemDelete(index){
         //react是immutable，state不允许我们做任何改变，只能复制之后改变副本
